3b8f2ae8fe5c2c0f15188801aaad5a592beea7d1
Steve Cooper <steve.cooper@anaplan.com>
2019-12-18 11:34:23 +0000

    cleaner tests

--- src/idl_parser.rs
+++ src/idl_parser.rs
@@ -3,8 +3,8 @@ extern crate pom;
 use pom::parser::*;
 //use pom::parser::Parser;
 use self::pom::char_class::{alpha, alphanum, multispace};
-use std::path::Path;
 use failure::Error;
+use std::path::Path;
 
 #[derive(Debug, PartialEq)]
 pub struct Interface {
@@ -21,7 +21,7 @@ pub struct AttributeDef {
 
 #[derive(Debug, PartialEq)]
 pub enum Member {
-    Attribute(AttributeDef)
+    Attribute(AttributeDef),
 }
 
 fn space<'a>() -> Parser<'a, u8, ()> {
@@ -42,11 +42,13 @@ fn member<'a>() -> Parser<'a, u8, Member> {
 
     let member_raw = readonly - attribute + typ + nam - space() - sym(b';');
 
-    member_raw.map(move |((readonly, typ), name)| Member::Attribute(AttributeDef {
-        readonly,
-        name,
-        typ,
-    }))
+    member_raw.map(move |((readonly, typ), name)| {
+        Member::Attribute(AttributeDef {
+            readonly,
+            name,
+            typ,
+        })
+    })
 }
 
 fn member_list<'a>() -> Parser<'a, u8, Vec<Member>> {
@@ -59,86 +61,103 @@ fn interface<'a>() -> Parser<'a, u8, Interface> {
     let interface_close = space() - sym(b'}') - space() - sym(b';');
     let interface = interface_start + member_list() - interface_close;
 
-    interface.map(|(name,members)| Interface {
-        name,
-        members
-    })
+    interface.map(|(name, members)| Interface { name, members })
 }
 
 fn idl<'a>() -> Parser<'a, u8, Vec<Interface>> {
     interface().repeat(0..)
 }
 
-
 #[cfg(test)]
 mod test {
     use super::*;
     use std::error::Error;
-    use std::path::Path;
     use std::fmt::Display;
+    use std::path::Path;
 
     macro_rules! assert_consumes_all {
         ( $ parser: expr, $input: expr ) => {
-            let terminating_parser = $ parser - space() - end();
+            let terminating_parser = $parser - space() - end();
             let res = terminating_parser.parse($input);
             if let Err(e) = res {
-                panic ! ("parser failed to match and consume everthing")
+                panic!("parser failed to match and consume everthing")
             }
-        }
+        };
+        ( $ parser: expr, $input: expr, $expected: expr) => {
+            let terminating_parser = $parser - space() - end();
+            let res = terminating_parser.parse($input);
+            match res {
+                Ok(answer) => {
+                    // it parsed, but was it right?
+                    assert_eq!(answer, $expected)
+                }
+                Err(e) => panic!("parser failed to match and consume everthing"),
+            }
+        };
     }
-
 
     #[test]
     fn parse_keywords() {
+        assert_consumes_all![space(), b""];
+        assert_consumes_all![space(), b"  "];
+        assert_consumes_all![space(), b"  \t \n \r "];
 
-        assert_consumes_all![ space(), b"" ];
-        assert_consumes_all![ space(), b"  " ];
-        assert_consumes_all![ space(), b"  \t \n \r " ];
-        assert_consumes_all!( member(), b"attribute DOMString value;");
-        assert_consumes_all!( member(), b"readonly attribute DOMString value;");
-        assert_consumes_all![ interface(), b"interface foo {};"];
-        assert_consumes_all!( interface(), b"interface foo { attribute DOMString value; };");
+        assert_consumes_all!(
+            member(),
+            b"attribute DOMString value;",
+            Member::Attribute(AttributeDef {
+                readonly: false,
+                name: String::from("value"),
+                typ: String::from("DOMString"),
+            })
+        );
 
-
-
-        assert_eq!(Ok(Member::Attribute(AttributeDef {
-            readonly: true,
-            name: String::from("value"),
-            typ: String::from("DOMString"),
-        })), member().parse(b"readonly attribute DOMString value;"));
-
-        let expected = vec![
+        assert_consumes_all!(
+            member(),
+            b"readonly attribute DOMString value;",
             Member::Attribute(AttributeDef {
                 readonly: true,
                 name: String::from("value"),
                 typ: String::from("DOMString"),
-            }),
-            Member::Attribute(AttributeDef {
-                readonly: false,
-                name: String::from("body"),
-                typ: String::from("HTMLElement"),
-            }),
+            })
+        );
+
+        assert_consumes_all![
+            interface(),
+            b"interface foo {};",
+            Interface {
+                name: String::from("foo"),
+                members: vec![]
+            }
         ];
+        assert_consumes_all!(
+            interface(),
+            b"interface foo { attribute DOMString value; };"
+        );
 
-        assert_eq!(member_list().parse(r"
+        assert_consumes_all!(
+            member_list(),
+            b"
 readonly attribute DOMString value;
-attribute HTMLElement body;".as_bytes()), Ok(expected));
-
-
-    }
-
-    #[test]
-    fn parse_idl() {
-        assert!(interface().parse(b"interface Window {};").is_ok());
+attribute HTMLElement body;",
+            vec![
+                Member::Attribute(AttributeDef {
+                    readonly: true,
+                    name: String::from("value"),
+                    typ: String::from("DOMString"),
+                }),
+                Member::Attribute(AttributeDef {
+                    readonly: false,
+                    name: String::from("body"),
+                    typ: String::from("HTMLElement"),
+                }),
+            ]
+        );
     }
 
     #[test]
     fn parse_files() {
-        let byte_vec: Vec<u8> = match std::fs::read("assets/valid_so_far.idl") {
-            Ok(x) => x,
-            Err(_) => panic!["nope"]
-        };
-
+        let byte_vec: Vec<u8> = std::fs::read("assets/valid_so_far.idl").unwrap();
         let byte_slice: &[u8] = &byte_vec;
         let idl_parser = idl() - space() - end();
         let parse_result = idl_parser.parse(byte_slice).unwrap();
